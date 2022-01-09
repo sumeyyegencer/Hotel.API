7eb682991fad034a4a69dac10320f1cf503cb30e
sumeyyegencer <sumeyyegencer@gmail.com>
2022-01-09 22:32:39 +0300

    App added.

---
+++ HotelFinder.DataAccess/HotelDbContext.cs
@@ -0,0 +1,24 @@
+﻿using HotelFinder.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace HotelFinder.DataAccess
+{
+    //Burada entity framework işlemlerini yapacağız.Bu yüzden dbcontext nesnesi gerekiyor.
+    class HotelDbContext:DbContext
+    {
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Server=.; Database=HotelDb; Trusted_Connection=True;");
+        }
+
+        public DbSet<Hotel> Hotels { get; set; }
+
+
+    }
+}
